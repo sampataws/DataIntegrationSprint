6b8f47fa57a1e8a79a8985c950a6c7b47d7b2e2b
Amber Vaid <ambervaid09@gmail.com>
2022-01-26 14:14:04 +0530

    Setup for example

---
+++ components/database/src/main/scala/com/dataintegration/database/Driver.scala
@@ -0,0 +1,25 @@
+package com.dataintegration.database
+
+import com.dataintegration.core.binders.IntegrationConf
+import com.dataintegration.core.impl.adapter.IntegrationClient
+import com.dataintegration.core.impl.services.Driver.{ComputeContract, JobContract, StorageContract}
+import com.dataintegration.core.services.configuration.Configuration
+import com.dataintegration.core.services.log.audit.DatabaseService
+import com.dataintegration.database.services.{AuditApi, CreateConnection}
+import zio.{ZEnv, ZIO, ZIOAppArgs, ZLayer}
+
+object Driver extends IntegrationClient with Configuration {
+
+  override val auditLayer: ZLayer[IntegrationConf, Nothing, DatabaseService.AuditTableApi] = {
+    for {
+      config <- ZIO.service[IntegrationConf]
+    } yield AuditApi(config.getProperties)
+  }.toLayer
+
+  override def run: ZIO[ZEnv with ZIOAppArgs, Any, Any] = builder(
+    configLayer = configLayer,
+    computeContract = ComputeContract,
+    storageContract = StorageContract,
+    jobContract = JobContract
+  ).provideLayer(CreateConnection.live)
+}
